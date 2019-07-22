d884f50e12dff2f9071239a9a95b5239c7b980ee
Adam Wilczyński <a9c2xx@gmail.com>
2019-07-22 16:42:30 +0200

    Complete project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-Project created for Web Development 101 course on theodinproject.com by A9C2
-You can see how it works here: https://a9c2.github.io/rock-paper-scissors/
+Project created for Web Development 101 course on theodinproject.com by A9C2.
+You can see how it works here: https://a9c2.github.io/rock-paper-scissors-2.0/
+
+Used assets:
+https://icons8.com/icon/10272/hand
+https://icons8.com/icon/41458/hand-rock
+https://icons8.com/icon/41456/hand-scissors
+https://icons8.com/icon/11684/restart
--- game.js
+++ game.js
@@ -1,70 +1,102 @@
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const restart = document.querySelector("#restart")
+rockButton.addEventListener("click", select);
+paperButton.addEventListener("click", select);
+scissorsButton.addEventListener("click", select);
+restart.addEventListener("click", restartGame)
+
+const result = document.querySelector("#result");
+const userScore = document.querySelector("#user-score");
+const computerScore = document.querySelector("#computer-score");
+const winner = document.querySelector("#winner")
+
+let breaker = false
+
+function restartGame() {
+    computerScore.innerHTML = 0;
+    userScore.innerHTML = 0;
+    winner.innerHTML = "";
+    result.innerHTML = "Whoever gets 5 points wins!";
+    breaker = false;
+    restart.style.width = "100px";
+    restart.style.height = "100px";
+}
+
+function select(e) {
+    playRound(e.target.id, computerPlay())
+}
+
+
+
 function computerPlay() {
     let plays = ["Rock", "Paper", "Scissors"];
     return plays[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
+    if (breaker) {
+        return;
+    }
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-
+    console.log(`Player selection: ${playerSelection}`);
+    console.log(`Computer selection: ${computerSelection}`);
     function lose(winning, losing) {
-        return `You Lose! ${winning.charAt(0).toUpperCase() + winning.slice(1)} ${winning === "scissors" ? "beat" : "beats"} ${losing.charAt(0).toUpperCase() + losing.slice(1)}`
+        computerScore.innerHTML = parseInt(computerScore.innerHTML, 10) + 1;
+        return `You Lose! ${winning.charAt(0).toUpperCase() + winning.slice(1)} ${winning === "scissors" ? "beat" : "beats"} ${losing.charAt(0).toUpperCase() + losing.slice(1)}.`
     };
 
     function win(winning, losing) {
-        return `You Win! ${winning.charAt(0).toUpperCase() + winning.slice(1)} ${winning === "scissors" ? "beat" : "beats"} ${losing.charAt(0).toUpperCase() + losing.slice(1)}`;
+        userScore.innerHTML = parseInt(userScore.innerHTML, 10) + 1;
+        return `You Win! ${winning.charAt(0).toUpperCase() + winning.slice(1)} ${winning === "scissors" ? "beat" : "beats"} ${losing.charAt(0).toUpperCase() + losing.slice(1)}.`;
     }
 
-    if (playerSelection === computerSelection) {
-        return ["It's a draw!", 0];
-    } else if (playerSelection === "rock") {
-        if (computerSelection === "scissors") {
-            return [win(playerSelection, computerSelection), 1];
-        } else {
-            return [lose(computerSelection, playerSelection), -1];
-        }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            return [win(playerSelection, computerSelection), 1];
-        } else {
-            return [lose(computerSelection, playerSelection), -1];
-        }
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "paper") {
-            return [win(playerSelection, computerSelection), 1];
-        } else {
-            return [lose(computerSelection, playerSelection), -1];
-        }
-    } else {
-        alert("You have to select Rock, Paper or Scissors! Press F5 to reload the page.")
-        throw "You have to select Rock, Paper or Scissors!";
+    let selections = playerSelection.charAt(0) + computerSelection.charAt(0);
+
+    switch (selections) {
+        case "rp":
+            result.innerHTML = lose(computerSelection, playerSelection);
+            break;
+        case "rs":
+            result.innerHTML = win(playerSelection, computerSelection);
+            break;
+        case "pr":
+            result.innerHTML = win(playerSelection, computerSelection);
+            break;
+        case "ps":
+            result.innerHTML = lose(computerSelection, playerSelection);
+            break;
+        case "sr":
+            result.innerHTML = lose(computerSelection, playerSelection);
+            break;
+        case "sp":
+            result.innerHTML = win(playerSelection, computerSelection);
+            break;
+        default:
+            result.innerHTML = "It's a draw!";
+            break;
+    }
+
+    if (userScore.innerHTML == 5) {
+        winner.innerHTML = "Player wins!";
+        breaker = true;
+    } else if (computerScore.innerHTML == 5) {
+        winner.innerHTML = "Computer wins!";
+        breaker = true;
+    }
+    if (breaker) {
+        restart.style.width = "120px";
+        restart.style.height = "120px";
+        return;
     }
 }
 
 function game() {
-    alert("In the game you play 5 rounds against computer. After them your score will be displayed on the screen. Click OK to start. GLHF!")
     let playerScore = 0;
     let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper or Scissors?");
-        let roundResult = playRound(playerSelection, computerPlay());
-        console.log(roundResult[0]);
-        let whoWon = roundResult[1];
-        if (whoWon === 0) { //Draw
-            document.getElementById(`round${i+1}`).innerHTML += "It's a Draw!";
-            continue;
-        } else if (whoWon === 1) { //Player wins
-            playerScore += 1;
-            document.getElementById("player-score").innerHTML = `Player Score: ${playerScore}`;
-            document.getElementById(`round${i+1}`).innerHTML += "Player Wins!";
-        } else { //Computer wins
-            computerScore += 1;
-            document.getElementById("computer-score").innerHTML = `Computer Score: ${computerScore}`;
-            document.getElementById(`round${i+1}`).innerHTML += "Computer Wins!";
-        }
-    }
-    
-    alert(`Player scored ${playerScore} ${playerScore !== 1 ? "points" : "point"} and Computer scored ${computerScore} ${computerScore !== 1 ? "points" : "point"}. ${playerScore === computerScore ? "It's a Draw!" : playerScore > computerScore ? "Player Wins!" : "Computer Wins!"}`)
+    let playerSelection;
 }
 
 game();
